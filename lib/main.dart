7290f13c6faa11b649f24ab6608014b9c98bbe11
mari2t <subcodeoft@gmail.com>
2024-09-21 12:31:19 +0900

    chore : main.dartに書いてあるコードの確認

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+// MyApp で定義したアプリの実行を Flutter に指示。
 void main() {
   runApp(MyApp());
 }
 
+// ウィジェットは、すべての Flutter アプリを作成する際の元になる要素。
+// このアプリ自体がウィジェット。
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -25,10 +28,21 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// MyAppState クラスでアプリの状態を定義
+// ChangeNotifier 自身の変更に関する通知を行うことができる
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  // ↓ Add this.
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
+//  build() 周囲の状況が変化するたびに自動的に呼び出される
+// watch アプリの現在の状態に対する変更を追跡
+// Column 任意の数の子を従え、それらを上から下へ一列に配置
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -42,7 +56,7 @@ class MyHomePage extends StatelessWidget {
           // ↓ Add this.
           ElevatedButton(
             onPressed: () {
-              print('button pressed!');
+              appState.getNext(); // ← This instead of print().
             },
             child: Text('Next'),
           ),
