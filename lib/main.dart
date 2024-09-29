97f9a447f227c4f9f7f495388672842feca9f900
mari2t <subcodeoft@gmail.com>
2024-09-29 13:56:55 +0900

    chore : 完成

--- lib/main.dart
+++ lib/main.dart
@@ -123,7 +123,7 @@ class _MyHomePageState extends State<MyHomePage> {
         break;
       case 1:
         //Placeholder():配置した場所に十字が入った四角形を描画して、その部分の UI が未完成であることを示すウィジェット
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -246,6 +246,36 @@ class BigCard extends StatelessWidget {
           semanticsLabel: "${pair.first} ${pair.second}",
         ),
       ),
+    );
+  }
+}
+
+// ...
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
     );
   }
 }
